bc19be4f1616af97dd54170a3fc61006412d9569
Dominicguri <dominic.guri@gmail.com>
2016-05-03 21:55:54 -0400

    agent class for partcle moving based on a default nonlinear diff eqn, or a custom one

--- sample_particle.m
+++ sample_particle.m
@@ -2,6 +2,7 @@
 %       Van Der Pol Oscillator (with External Force)
 %       Duffing Oscillator
 %       Van Der Pol Oscillator
+clear
 
 %% http://www.sharetechnote.com/html/Octave_Matlab_DifferentialEquation.html#Ode45_Vander_Pol_Oscillator_ExternalForce
 mu = 1;
@@ -13,13 +14,20 @@ dy_dt = @(t,y) [y(2);...
                 mu*(1-y(1)^2)*y(2)-k*y(1) + A*sin(w*t)];
             
 odeopt = odeset ('RelTol', 0.00001, 'AbsTol', 0.00001,'InitialStep',0.5,'MaxStep',0.5);
-[t,y] = ode45(dy_dt,[0 40], [1.0 2.0],odeopt);
+% [t,y] = ode45(dy_dt,[0 40], [1.0 2.0],odeopt);
+[t,y] = ode45(dy_dt,(0:0.01:40), [1.0 2.0],odeopt);
 
 figure(1); clf
+
 subplot(1,3,[1 2]);
-plot(t,y(:,1),'r-',t,y(:,2),'g-'); xlabel('time'); legend('y(1)','y(2)');
+plot(t,y(:,1),'r-',t,y(:,2),'g-')
+xlabel('time'); legend('y(1)','y(2)');
+grid on
+
 subplot(1,3,3);
-plot(y(:,1),y(:,2)); xlabel('y(1)'); ylabel('y(2)');
+plot(y(:,1),y(:,2))
+xlabel('y(1)'); ylabel('y(2)');
+grid on
 
 %% http://www.sharetechnote.com/html/Octave_Matlab_DifferentialEquation.html#Ode45_Duffing_Oscillator
 delta = 0.06;
